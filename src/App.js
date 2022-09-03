d350fd94aba2e99f0a1daeb715e1de6153d8869d
MarcellaFox <marcella.fox@outlook.com>
2022-09-03 12:12:12 +1000

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import "bootstrap-css";
+import Dictionary from "./Dictionary";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="app-footer">Coded by Marcella Fox.</footer>
+    </div>
+  );
 }
 
 export default App;
