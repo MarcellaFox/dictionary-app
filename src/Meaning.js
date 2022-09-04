edbcfd1187146cbfb034c8834b7025b1f43d7b41
MarcellaFox <marcella.fox@outlook.com>
2022-09-04 13:01:58 +1000

    Add synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -9,8 +10,9 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              {definition.definition} <br />
-              <em>{definition.example}</em>
+              <p>{definition.definition}</p> <br />
+              <em>{definition.example}</em> <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
