e2628aafb292afa27cd7a3079e836353606b4352
MarcellaFox <marcella.fox@outlook.com>
2022-09-04 14:22:29 +1000

    Create Phonetics component

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,18 +2,16 @@ import React from "react";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
+  // console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <p>{definition.definition}</p> <br />
-              <em>{definition.example}</em> <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <p>{definition.definition}</p> <br />
+            <em>{definition.example}</em> <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
